d8f45b9cacd8d4ab786a114f7caff767ece2fd5f
Kanna Kim <kimkanna18@gmail.com>
2021-12-23 19:24:05 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditPostComponent } from './edit-post/edit-post.component';
+import { MidComponent } from './mid/mid.component';
+import { PostViewComponent } from './post-view/post-view.component';
+import { PostingComponent } from './posting/posting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MidComponent},
+  {path: 'posting', component: PostingComponent},
+  {path: 'view', component: PostViewComponent},
+  {path: 'edit', component: EditPostComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { MidComponent } from './mid/mid.component';
+import { BotComponent } from './bot/bot.component';
+import { PostingComponent } from './posting/posting.component';
+import { NgxEditorModule } from 'ngx-editor';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { PostViewComponent } from './post-view/post-view.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    MidComponent,
+    BotComponent,
+    PostingComponent,
+    PostViewComponent,
+    EditPostComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgxEditorModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
